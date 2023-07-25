487abb4459c9f093103fae530459d06f359cff21
MonokaiJs <monokaijs@gmail.com>
2023-07-25 07:01:55 +0700

    :fire: add menu hamburger

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import {useThemeContext} from "@/components/providers/ThemeProvider";
+import Hamburger from "@/assets/icons/Hamburger";
+import {NavigationConfig} from "@/configs/app.config";
 
 export default function AppHeader() {
   const {heroColors} = useThemeContext();
@@ -14,17 +16,13 @@ export default function AppHeader() {
       NorthStudio
     </div>
     <div className={styles.links}>
-      <a>
-        Home
-      </a>
-      <a>
-        Story
-      </a>
-      <a>
-        Career
-      </a>
-      <a>
-        Contact
+      {NavigationConfig.map(item => (
+        <a href={item.url} key={item.key} style={{color: heroColors.text}}>
+          {item.title}
+        </a>
+      ))}
+      <a className={styles.hamburgerBtn}>
+        <Hamburger color={heroColors.text}/>
       </a>
     </div>
   </div>
---
+++ src/configs/app.config.ts
@@ -0,0 +1,17 @@
+export const NavigationConfig = [{
+  key: 'home',
+  title: 'Home',
+  url: '/'
+}, {
+  key: 'story',
+  title: 'Story',
+  url: '/story'
+}, {
+  key: 'career',
+  title: 'Career',
+  url: '/career'
+}, {
+  key: 'contact',
+  title: 'Contact',
+  url: '/contact'
+}]
