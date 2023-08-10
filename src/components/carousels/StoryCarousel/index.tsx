4cd155a519f530bbb971b60c22da130e75202690
Edward Nguyen <monokaijs@gmail.com>
2023-08-10 13:57:53 +0700

    :sparkles: story animation

--- src/components/carousels/StoryCarousel/index.tsx
+++ src/components/carousels/StoryCarousel/index.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Story01 from "@/assets/figures/story-01.jpg";
 import styles from './styles.module.scss';
 
-export default function StoryCarousel() {
-  return <div className={styles.container}>
+interface StoryCarouselProps {
+  activated: boolean;
+}
+
+export default function StoryCarousel({activated}: StoryCarouselProps) {
+  return <div
+    className={`${styles.container} ${activated ? styles.active : ''}`}
+  >
     <div className={styles.figureContainer}>
       <div
         className={styles.figure}
--- src/components/sections/Story.tsx
+++ src/components/sections/Story.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles from "./sections.module.scss";
 import SectionTitle from "@/components/shared/SectionTitle";
 import StoryCarousel from "@/components/carousels/StoryCarousel";
 
 export default function Story() {
-  return <div id={'story'} className={styles.section}>
+  const [activated, setActivated] = useState(false);
+  const outerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    window.addEventListener('scroll', e => {
+      if (outerRef.current?.offsetTop && outerRef.current?.offsetTop - window.scrollY < 300) {
+        console.log('activated');
+        setActivated(true);
+      }
+    });
+  }, []);
+
+  return <div
+    ref={outerRef}
+    id={'story'}
+    className={styles.section}
+  >
     <div className={styles.sectionContent}>
       <SectionTitle>
         Our Story
@@ -13,9 +28,9 @@ export default function Story() {
         Story of a journey...
       </div>
 
-      <StoryCarousel/>
+      <StoryCarousel activated={activated}/>
 
-      <div className={styles.storyText}>
+      <div className={`${styles.storyText} ${activated ? styles.storyTextActivated: ''}`}>
         <div className={styles.storyTextContent}>
           <p>
             Được thành lập cuối năm 2020, NorthStudio hình thành bởi một khát vọng mang lại những giá trị thực tiễn từ lòng nhiệt huyết, tinh thần trẻ và những đôi tay trẻ của những con người trẻ. Thời điểm mới thành lập, đội ngũ của “Những kẻ hướng về Phương Bắc” mới chỉ mười tám, mười chín tuổi, là những kẻ ngông cuồng tìm lấy cho mình một con đường đổi đời, một con đường mà ở đó những ý tưởng ấp ủ, những ước mơ được hoá thành hiện thực.
@@ -25,7 +40,7 @@ export default function Story() {
             Ở đây, chúng tôi không chỉ có những con người đang làm việc, đang hướng tới work-life-balance, chúng tôi có những con người sống được cùng công việc và biến được công việc trở thành một phần thú vị của cuộc sống. Vì, chúng tôi quan niệm rằng cuộc sống sẽ thật là vô nghĩa nếu không có công việc, không có những sự cống hiến và cuộc đời sẽ thật là vô ích nếu như không mang lại giá trị gì cho xã hội bên ngoài.
           </p>
         </div>
-        <div className={styles.storyTextTitle}>
+        <div className={`${styles.storyTextTitle} ${activated ? styles.storyTextTitleActivated : ''}`}>
           <h3>
             Câu chuyện về Con người
           </h3>
